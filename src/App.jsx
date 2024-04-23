3edd8ba8962dfc8da8a353315b86193689e4c3a3
Yoan Clergeot <yoan.clergeot@gmail.com>
2024-04-23 11:39:57 +0200

    sauvegarde

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import Block from './todo-list/src/components/ui/Block';
-import Tasks from './components/features/Tasks';
+import Tasks from "./components/features/Tasks";
+import Block from "./components/ui/Block";
 
 const App = () => {
-
-	return (
-		<div className="container">
-			<h1 className="text--primary">Todo</h1>
-			<Block>
-				<Tasks />
-			</Block>
-		</div>
-	)
+  return (
+    <div className="container">
+      <h1 className="text--primary">Todo</h1>
+      <Block>
+        <Tasks />
+      </Block>
+    </div>
+  );
 };
 
 export default App;
