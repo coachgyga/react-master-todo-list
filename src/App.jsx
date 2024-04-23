2d2a04311b82c9a9f9743a680ee31dac7398a215
Yoan <yclergeot@makheia.com>
2024-04-23 23:32:01 +0200

    Modifs exercice

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,57 @@
+import { useEffect, useRef, useState } from "react";
 import Tasks from "./components/features/Tasks";
 import Block from "./components/ui/Block";
+import Button from "./components/ui/Button";
 
+let nextId = 0;
 const App = () => {
+  const [tasks, setTasks] = useState([]);
+
+  const newTaskInputRef = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newTask = newTaskInputRef.current.value;
+    const date = new Date().toLocaleDateString();
+    nextId++;
+    // console.log(e.target.todoName.value);
+    setTasks([...tasks, { id: nextId, title: newTask, date: date }]);
+  };
+
+  useEffect(() => {
+    newTaskInputRef.current.value = "";
+  }, [tasks]);
+
   return (
     <div className="container">
       <h1 className="text--primary">Todo</h1>
+      <form
+        onSubmit={handleSubmit}
+        action=""
+        style={{
+          display: "flex",
+          gap: "10px",
+          marginBottom: "24px",
+          width: "100%",
+        }}
+      >
+        <input
+          type="text"
+          name="todoName"
+          placeholder="Entrez votre tâche"
+          ref={newTaskInputRef}
+          style={{
+            flexGrow: "1",
+            border: "0",
+            borderRadius: "5px",
+          }}
+        ></input>
+        <Button variant="primary">Create</Button>
+      </form>
+
       <Block>
-        <Tasks />
+        <Tasks tasks={tasks} />
       </Block>
     </div>
   );
--- src/components/features/Tasks/index.jsx
+++ src/components/features/Tasks/index.jsx
@@ -1,10 +1,48 @@
-const Tasks = () => {
+import Button from "../../ui/Button";
+import "./Tasks.css";
 
-	return (
-		<>
-			<p>Tasks component</p>
-		</>
-	);
+import { array } from "prop-types";
+
+const Tasks = ({ tasks }) => {
+  const handleDelete = () => {};
+  return (
+    <>
+      <table className="tasks-table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Create At</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tasks != "" ? (
+            tasks.map((task) => {
+              return (
+                <tr key={task.id}>
+                  <td>{task.title}</td>
+                  <td>{task.date}</td>
+                  <td>
+                    <Button onClick={() => handleDelete()} variant="danger">
+                      Delete
+                    </Button>
+                  </td>
+                </tr>
+              );
+            })
+          ) : (
+            <tr>
+              <td>no data</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </>
+  );
 };
 
 export default Tasks;
+
+Tasks.propTypes = {
+  tasks: array.isRequired,
+};
